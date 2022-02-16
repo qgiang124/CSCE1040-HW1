d75c3707622640587aed3e4179338429144a7703
qgiang124 <quynhgiangg124@gmail.com>
2022-02-16 16:34:25 -0600

    Initial Commit

---
+++ HW1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW1
+//
+//  Created by Giang Tran on 2/16/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
